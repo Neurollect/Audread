ed56a4f0a4bcb354b37e390f3f8acf937320dbb6
JosephGakah <joegakah@gmail.com>
2023-05-10 21:45:31 +0300

    Change attribute provider

--- lib/providers/account_details_provider.dart
+++ lib/providers/account_details_provider.dart
@@ -1,3 +1,19 @@
+import 'package:audread/models/member.dart';
 import 'package:flutter/material.dart';
 
-class AccountDetailsProvider with ChangeNotifier {}
+class AccountDetailsProvider with ChangeNotifier {
+  MemberModel member = MemberModel(
+    id: 'id',
+    firstName: 'Joseph',
+    lastName: 'Gakah',
+    organization: 'braintraq',
+    grade: 'Grade 12',
+    avatar: 4,
+    gender: 'Male',
+  );
+
+  changeAttribute(value) {
+    member.organization = value;
+    notifyListeners();
+  }
+}
