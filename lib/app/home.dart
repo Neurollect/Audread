a50e1e915f21600bf2894ba8dd30d1d1d62ac407
JosephGakah <joegakah@gmail.com>
2023-05-15 23:05:22 +0300

    search button

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,5 +1,7 @@
 import 'package:audread/app/widgets/header.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,13 +14,52 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     //double side = MediaQuery.of(context).size.width / 6;
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: EdgeInsets.all(30),
+          padding: const EdgeInsets.all(30),
           child: Column(
             children: [
-              AudHeader(),
+              const AudHeader(),
+              const SizedBox(height: 40),
+              TextFormField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                  label: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          ' Search ',
+                          style: GoogleFonts.urbanist(
+                            fontSize: 21,
+                          ),
+                        ),
+                        const Icon(Iconsax.search_normal),
+                      ],
+                    ),
+                  ),
+                  focusColor: Colors.black,
+                  enabledBorder: OutlineInputBorder(
+                    gapPadding: 2,
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 0,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
