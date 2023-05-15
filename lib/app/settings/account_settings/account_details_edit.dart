87a237384a2d8446452b77e88403cbb28f51188e
JosephGakah <joegakah@gmail.com>
2023-05-15 21:46:41 +0300

    Removing editing of attributes

--- lib/app/settings/account_settings/account_details_edit.dart
+++ lib/app/settings/account_settings/account_details_edit.dart
@@ -2,7 +2,6 @@ import 'package:audread/models/member.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:audread/providers/account_details_provider.dart';
 import 'package:audread/utils/utils.dart';
 
@@ -40,30 +39,6 @@ class AccountDetailsEditState extends State<AccountDetailsEdit> {
               children: [
                 for (var i in detailsItems) ...[
                   ListTile(
-                    trailing: TextButton(
-                      onPressed: () {
-                        if (isEditing) {
-                          provider.changeAttribute(i[0]);
-                          setState(() {
-                            isEditing = !isEditing;
-                          });
-                        } else {
-                          setState(() {
-                            isEditing = !isEditing;
-                          });
-                        }
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(31, 0, 145, 255),
-                        shape: const CircleBorder(),
-                        foregroundColor: const Color.fromARGB(255, 0, 36, 89),
-                      ),
-                      child: Icon(
-                        isEditing ? Icons.save_outlined : Iconsax.edit,
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                    ),
                     title: Text(
                       i[0],
                       style: GoogleFonts.urbanist(
