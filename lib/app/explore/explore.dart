ac122d40c514839aefbb6a82212e69e9876ac200
JosephGakah <joegakah@gmail.com>
2023-05-16 23:20:24 +0300

    Exploree itinary

---
+++ lib/app/explore/explore.dart
@@ -0,0 +1,41 @@
+import 'package:audread/app/explore/featured_topics.dart';
+import 'package:audread/app/widgets/bottom_nav.dart';
+import 'package:audread/utils/utils.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/header.dart';
+
+final utils = Utils();
+
+class ExploreTopicsPage extends StatefulWidget {
+  const ExploreTopicsPage({Key? key}) : super(key: key);
+
+  @override
+  ExploreTopicsPageState createState() => ExploreTopicsPageState();
+}
+
+class ExploreTopicsPageState extends State<ExploreTopicsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const AudHeader(),
+              const SizedBox(height: 20),
+              TextFormField(
+                textAlign: TextAlign.center,
+                decoration: utils.inputFields.searchButtonDecoration(context),
+              ),
+              const SizedBox(height: 20),
+              const FeaturedTopics(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNav(),
+    );
+  }
+}
