c31c75ef89f4d24ac7671bc20b6d7160ca8010f0
JosephGakah <joegakah@gmail.com>
2023-05-16 15:30:25 +0300

    For you section and Audlabs

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,8 +1,8 @@
+import 'package:audread/app/home/for_you_section.dart';
 import 'package:audread/app/widgets/header.dart';
 import 'package:audread/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:iconsax/iconsax.dart';
 
 final utils = Utils();
 
@@ -83,95 +83,58 @@ class HomePageState extends State<HomePage> {
               const SizedBox(height: 30),
 
               //For You Section
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'For you',
-                    style: Theme.of(context).textTheme.labelLarge,
+              const ForYouSection(),
+
+              //AudLabs Container
+              Container(
+                padding: const EdgeInsets.all(15),
+                width: double.infinity,
+                height: height * 1.1,
+                decoration: BoxDecoration(
+                  color: Colors.red.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/home/ar.png'),
+                    alignment: Alignment.bottomRight,
                   ),
-                  Text(
-                    '-See All',
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Colors.greenAccent.shade700,
-                            borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Join AudLabs to Elevate Your Learning with\nAR ',
+                            style: Theme.of(context).textTheme.labelLarge,
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Introduction',
-                                style: GoogleFonts.urbanist(
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                ),
+                          const SizedBox(height: 10),
+                          TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.red,
+                              padding: const EdgeInsets.all(6),
+                            ),
+                            child: Text(
+                              'Join AudLabs',
+                              style: GoogleFonts.urbanist(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
                               ),
-                              const SizedBox(height: 8),
-                              Text(
-                                'Excretion and Homeostasis',
-                                style: GoogleFonts.urbanist(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                'Excretion is a process by which living organisms separate..',
-                                style: GoogleFonts.urbanist(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '20 Min',
-                                    style: GoogleFonts.urbanist(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {},
-                                    style: TextButton.styleFrom(
-                                      backgroundColor: Colors.white,
-                                      foregroundColor: Colors.black,
-                                      shape: const CircleBorder(),
-                                    ),
-                                    child: const Icon(Iconsax.play),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  const Expanded(flex: 1, child: SizedBox())
-                ],
+                    const Expanded(flex: 3, child: SizedBox()),
+                  ],
+                ),
               ),
+              const SizedBox(height: 30),
             ],
           ),
         ),
