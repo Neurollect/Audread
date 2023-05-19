83c745e58113e341469ec1f708436e07f6dc5087
JosephGakah <joegakah@gmail.com>
2023-05-19 14:10:40 +0300

    header update. Including a drawer display button.

--- lib/app/widgets/header.dart
+++ lib/app/widgets/header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 
 class AudHeader extends StatefulWidget {
   const AudHeader({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class AudHeader extends StatefulWidget {
 class AudHeaderState extends State<AudHeader> {
   @override
   Widget build(BuildContext context) {
-    double side = MediaQuery.of(context).size.width / 6;
+    double side = MediaQuery.of(context).size.width / 8;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -20,32 +21,60 @@ class AudHeaderState extends State<AudHeader> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '22 Oct',
-              style: GoogleFonts.urbanist(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
               'Hi, Joseph',
               style: GoogleFonts.urbanist(
                 fontSize: 35,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Text(
+              '22 Oct',
+              style: GoogleFonts.urbanist(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ],
         ),
-        Container(
-          height: side,
-          width: side,
-          alignment: Alignment.bottomCenter,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: const DecorationImage(
-              opacity: 0.8,
-              image: AssetImage('assets/images/avatars/mikee.png'),
+        const Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: side * 0.9,
+              width: side * 0.9,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  opacity: 0.8,
+                  image: AssetImage('assets/images/avatars/mikee.png'),
+                ),
+              ),
             ),
-          ),
+            const SizedBox(width: 10),
+            Container(
+              height: side * 0.8,
+              width: side * 0.8,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  width: 1.2,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                ),
+                child: Icon(
+                  Iconsax.microscope,
+                  size: side * 0.7,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
