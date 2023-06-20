214998a6edc2875db7d4be7fcc612371c40e5abe
JosephGakah <joegakah@gmail.com>
2023-06-20 13:06:38 +0300

    Structural changes on subject header view

--- lib/app/subject/grade/subject_header.dart
+++ lib/app/subject/grade/subject_header.dart
@@ -28,65 +28,66 @@ class SubjectHeaderState extends State<SubjectHeader> {
   @override
   Widget build(BuildContext context) {
     double side = MediaQuery.of(context).size.width / 8;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (widget.subject == null) ...[
-              Expanded(
-                child: CardLoading(
-                  height: 30,
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  margin: const EdgeInsets.only(top: 12),
-                  cardLoadingTheme: cardLoadingTheme,
-                ),
-              ),
-            ] else ...[
-              Text(
-                widget.subject.toString(),
-                style: GoogleFonts.urbanist(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Science & Technology',
-                style: GoogleFonts.urbanist(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ]
-          ],
+        Container(
+          height: side * 0.9,
+          width: side * 0.9,
+          decoration: BoxDecoration(
+            color: Colors.blue.shade50,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: TextButton(
+            onPressed: () => Get.back(),
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.zero,
+            ),
+            child: Icon(
+              Icons.arrow_back,
+              size: side * 0.7,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
         ),
-        const Spacer(),
+        const SizedBox(height: 25),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              height: side * 0.9,
-              width: side * 0.9,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade50,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: () => Get.back(),
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                ),
-                child: Icon(
-                  Icons.arrow_back,
-                  size: side * 0.7,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (widget.subject == null) ...[
+                  Expanded(
+                    child: CardLoading(
+                      height: 30,
+                      borderRadius: const BorderRadius.all(Radius.circular(4)),
+                      margin: const EdgeInsets.only(top: 12),
+                      cardLoadingTheme: cardLoadingTheme,
+                    ),
+                  ),
+                ] else ...[
+                  Text(
+                    widget.subject.toString(),
+                    style: GoogleFonts.urbanist(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Science & Technology',
+                    style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ]
+              ],
             ),
           ],
-        ),
+        )
       ],
     );
   }
