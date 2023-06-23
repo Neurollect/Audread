42a15e9c5a368fd72f04a0ff12cb724059a6b2f2
JosephGakah <joegakah@gmail.com>
2023-06-23 15:57:07 +0300

    Video player and internet access.

--- lib/app/subject/topic/topic_prov.dart
+++ lib/app/subject/topic/topic_prov.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:provider/provider.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../widgets/continue_last_lesson.dart';
 import 'next_topic.dart';
@@ -20,6 +21,19 @@ class TopicProv extends StatefulWidget {
 }
 
 class TopicProvState extends State<TopicProv> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
   displayDialog(anyFunction) {
     SchedulerBinding.instance.addPostFrameCallback((_) {
       Future.delayed(const Duration(milliseconds: 1000), () {
@@ -74,6 +88,13 @@ class TopicProvState extends State<TopicProv> {
                           if (provider.isContinueFromPrevAvailable) ...[
                             const ContinueLastLesson(),
                           ],
+
+                          _controller.value.isInitialized
+                              ? AspectRatio(
+                                  aspectRatio: _controller.value.aspectRatio,
+                                  child: VideoPlayer(_controller),
+                                )
+                              : Container(),
 
                           //Subtopics List
                           SubtopicsList(topicId: topic.topicId),
--- lib/app/widgets/navigation_drawer.dart
+++ lib/app/widgets/navigation_drawer.dart
@@ -1,8 +1,4 @@
-import 'package:audread/app/home.dart';
-import 'package:audread/app/settings/profile_settings.dart';
-import 'package:audread/app/settings/settings.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -25,42 +21,6 @@ class NavDrawer extends StatelessWidget {
       ['Settings', Iconsax.setting_3],
       ['BookMarks', Iconsax.bookmark_2],
     ];
-
-    navigateToPage(String page) {
-      switch (page) {
-        case 'Audread- Home':
-          Navigator.pop(context);
-          Get.to(const HomePage());
-          break;
-        case 'AudLabs':
-          Navigator.pop(context);
-          break;
-        case 'AudCoterie- Chat':
-          Navigator.pop(context);
-          break;
-        case 'AudTube':
-          Navigator.pop(context);
-          break;
-        case 'Help Center':
-          Navigator.pop(context);
-          break;
-        case 'Profile':
-          Navigator.pop(context);
-          Get.to(const ProfileSettings());
-          break;
-        case 'Notifications':
-          Navigator.pop(context);
-          break;
-        case 'Settings':
-          Navigator.pop(context);
-          Get.to(const SettingsScreen());
-          break;
-        case 'BookMarks':
-          Navigator.pop(context);
-          break;
-        default:
-      }
-    }
 
     return Drawer(
       width: MediaQuery.of(context).size.width * 1,
@@ -85,7 +45,9 @@ class NavDrawer extends StatelessWidget {
               children: [
                 for (var i in menuItems) ...[
                   TextButton(
-                    onPressed: navigateToPage(i[0]),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                     style: TextButton.styleFrom(
                       padding: EdgeInsets.zero,
                     ),
@@ -131,7 +93,7 @@ class NavDrawer extends StatelessWidget {
               children: [
                 for (var i in lowerItems) ...[
                   TextButton(
-                    onPressed: navigateToPage(i[0]),
+                    onPressed: () {},
                     style: TextButton.styleFrom(
                       padding: EdgeInsets.zero,
                     ),
